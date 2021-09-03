a5540597b86645e956e18131af1b545d036c2038
Dominik Mocher <dominik.mocher@a-sit.at>
2021-09-03 10:32:11 +0200

    Provide encryption parameter on service initialisation

--- Sources/DccCachingService/DccCachingService.swift
+++ Sources/DccCachingService/DccCachingService.swift
@@ -25,49 +25,55 @@ public class DccCachingService {
     public var valueSetService : ValueSetsService
     public var businessRulesService : BusinessRulesService
     
-    public init(dateService: DateService? = nil, trustlistUrl : String? = nil, trustlistSignatureUrl: String? = nil, trustlistTrustAnchor: String? = nil, trustlistApiKey: String? = nil,
-                businessRulesUrl: String? = nil, businessRulesSignatureUrl: String? = nil, businessRulesTrustAnchor : String? = nil, businessRulesApiKey: String? = nil,
-                valueSetUrl: String? = nil, valueSetSignatureUrl: String? = nil, valueSetTrustAnchor: String? = nil, valueSetApiKey: String? = nil){
+    public init(dateService: DateService? = nil, trustlistUrl : String? = nil, trustlistSignatureUrl: String? = nil, trustlistTrustAnchor: String? = nil, trustlistApiKey: String? = nil, encryptTrustlist: Bool = false,
+                businessRulesUrl: String? = nil, businessRulesSignatureUrl: String? = nil, businessRulesTrustAnchor : String? = nil, businessRulesApiKey: String? = nil, encryptBusinessRules: Bool = false,
+                valueSetUrl: String? = nil, valueSetSignatureUrl: String? = nil, valueSetTrustAnchor: String? = nil, valueSetApiKey: String? = nil, encryptValueSet: Bool = false){
         self.trustlistService = DefaultTrustlistService(dateService: dateService ?? DefaultDateService(),
                                                         trustlistUrl: trustlistUrl ?? DccCachingService.DEFAULT_TRUSTLIST_URL,
                                                         signatureUrl: trustlistSignatureUrl ?? DccCachingService.DEFAULT_SIGNATURE_URL,
                                                         trustAnchor: trustlistTrustAnchor ?? DccCachingService.DEFAULT_TRUSTLIST_TRUSTANCHOR,
-                                                        apiKey: trustlistApiKey)
+                                                        apiKey: trustlistApiKey,
+                                                        storeEncrypted: encryptTrustlist)
         
         self.businessRulesService = DefaultBusinessRulesService(dateService: dateService ?? DefaultDateService(),
                                                                 businessRulesUrl: businessRulesUrl ?? DccCachingService.DEFAULT_BUSINESS_RULES_URL,
                                                                 signatureUrl: businessRulesSignatureUrl ?? DccCachingService.DEFAULT_BUSINESS_RULES_SIGNATURE_URL,
                                                                 trustAnchor: businessRulesTrustAnchor ?? DccCachingService.DEFAULT_BUSINESS_RULES_TRUSTANCHOR,
-                                                                apiKey: businessRulesApiKey)
+                                                                apiKey: businessRulesApiKey,
+                                                                storeEncrypted: encryptBusinessRules)
         
         self.valueSetService = DefaultValueSetsService(dateService: dateService ?? DefaultDateService(),
                                                        valueSetsUrl: valueSetUrl ?? DccCachingService.DEFAULT_VALUE_SET_URL,
                                                        signatureUrl: valueSetSignatureUrl ?? DccCachingService.DEFAULT_VALUE_SET_SIGNATURE_URL,
                                                        trustAnchor: valueSetTrustAnchor ?? DccCachingService.DEFAULT_VALUE_SET_TRUSTANCHOR,
-                                                       apiKey: valueSetApiKey)
+                                                       apiKey: valueSetApiKey,
+                                                       storeEncrypted: encryptValueSet)
     }
     
-    public func initTrustlistService(trustlistUrl : String? = nil, signatureUrl: String? = nil, trustAnchor: String? = nil, apiKey: String? = nil, dateService: DateService? = nil) {
+    public func initTrustlistService(trustlistUrl : String? = nil, signatureUrl: String? = nil, trustAnchor: String? = nil, apiKey: String? = nil, dateService: DateService? = nil, storeEncrypted: Bool = false) {
         self.trustlistService = DefaultTrustlistService(dateService: dateService ?? DefaultDateService(),
                                                         trustlistUrl: trustlistUrl ?? DccCachingService.DEFAULT_TRUSTLIST_URL,
                                                         signatureUrl: signatureUrl ?? DccCachingService.DEFAULT_SIGNATURE_URL,
                                                         trustAnchor: trustAnchor ?? DccCachingService.DEFAULT_TRUSTLIST_TRUSTANCHOR,
-                                                        apiKey: apiKey)
+                                                        apiKey: apiKey,
+                                                        storeEncrypted: storeEncrypted)
     }
     
-    public func initBusinessRuleService(businessRulesUrl: String? = nil, signatureUrl: String? = nil, trustAnchor: String? = nil, apiKey: String? = nil, dateService: DateService? = nil) {
+    public func initBusinessRuleService(businessRulesUrl: String? = nil, signatureUrl: String? = nil, trustAnchor: String? = nil, apiKey: String? = nil, dateService: DateService? = nil, storeEncrypted: Bool = false) {
         self.businessRulesService = DefaultBusinessRulesService(dateService: dateService ?? DefaultDateService(),
                                                                 businessRulesUrl: businessRulesUrl ?? DccCachingService.DEFAULT_BUSINESS_RULES_URL,
                                                                 signatureUrl: signatureUrl ?? DccCachingService.DEFAULT_BUSINESS_RULES_SIGNATURE_URL,
                                                                 trustAnchor: trustAnchor ?? DccCachingService.DEFAULT_BUSINESS_RULES_TRUSTANCHOR,
-                                                                apiKey: apiKey)
+                                                                apiKey: apiKey,
+                                                                storeEncrypted: storeEncrypted)
     }
     
-    public func initValueSetService(valueSetUrl: String? = nil, signatureUrl: String? = nil, trustAnchor: String? = nil, apiKey: String? = nil, dateService: DateService? = nil) {
+    public func initValueSetService(valueSetUrl: String? = nil, signatureUrl: String? = nil, trustAnchor: String? = nil, apiKey: String? = nil, dateService: DateService? = nil, storeEncrypted: Bool = false) {
         self.valueSetService = DefaultValueSetsService(dateService: dateService ?? DefaultDateService(),
                                                        valueSetsUrl: valueSetUrl ?? DccCachingService.DEFAULT_VALUE_SET_URL,
                                                        signatureUrl: signatureUrl ?? DccCachingService.DEFAULT_VALUE_SET_SIGNATURE_URL,
                                                        trustAnchor: trustAnchor ?? DccCachingService.DEFAULT_VALUE_SET_TRUSTANCHOR,
-                                                       apiKey: apiKey)
+                                                       apiKey: apiKey,
+                                                       storeEncrypted: storeEncrypted)
     }
 }
